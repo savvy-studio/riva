a63454a1ce0777e482a4e5491bac2d3ff0338b21
Luis Benitez <luisbttf@gmail.com>
2017-01-30 10:38:52 -0600

    menu funcional

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,3 +6,26 @@ $(document).scroll(function () {
         $('section').removeClass("height");
     }
 });
+  $(function() {
+
+    $('section.mobile-nav').on("click", function(evento){
+      $('.section.mobile-nav-click').css( 'display', 'none' );
+    });
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+
+
+
+
+  });
